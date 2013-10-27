90ee8b0e57f57a43893331b4523ba5a3fd945a03
Matej Kramny <sourcetree@matej.me>
2013-10-27 22:10:40 +0000

    Initial Commit

---
+++ iOrder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOrder
+//
+//  Created by Matej Kramny on 27/10/2013.
+//  Copyright (c) 2013 Matej Kramny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ iOrderTests/iOrderTests.m
@@ -0,0 +1,34 @@
+//
+//  iOrderTests.m
+//  iOrderTests
+//
+//  Created by Matej Kramny on 27/10/2013.
+//  Copyright (c) 2013 Matej Kramny. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOrderTests : XCTestCase
+
+@end
+
+@implementation iOrderTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
